038118f92935c8806dfe8458030128e11cd79965
vernetto <publicpierre@gmail.com>
2019-06-11 22:11:45 +0200

    server trusts client

--- src/org/pierre/sslserver/SSLServer.java
+++ src/org/pierre/sslserver/SSLServer.java
@@ -10,6 +10,8 @@ import java.net.Socket;
 
 public class SSLServer {
     public static void main(String[] args) throws IOException {
+        System.setProperty("javax.net.ssl.trustStore", "servertruststore.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword", "password");
         System.setProperty("javax.net.ssl.keyStore", "serverkeystore.jks");
         System.setProperty("javax.net.ssl.keyStorePassword", "password");
         System.setProperty("javax.net.debug", "ssl");
@@ -18,10 +20,8 @@ public class SSLServer {
         try (ServerSocket listener = factory.createServerSocket(port)) {
             SSLServerSocket sslListener = (SSLServerSocket) listener;
             sslListener.setNeedClientAuth(true);
-            sslListener.setEnabledCipherSuites(
-                    new String[] { "TLS_DHE_DSS_WITH_AES_256_CBC_SHA256" });
-            sslListener.setEnabledProtocols(
-                    new String[] { "TLSv1.2" });
+            sslListener.setEnabledCipherSuites(new String[] { "TLS_DHE_DSS_WITH_AES_256_CBC_SHA256" });
+            sslListener.setEnabledProtocols(new String[] { "TLSv1.2" });
             while (true) {
                 try (Socket socket = sslListener.accept()) {
                     PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
